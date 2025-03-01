dd52caa649b7b2fa92369361cb1f475047f9cd17
Solinur <github@my-eg0.de>
2025-03-01 14:36:00 +0100

    add mundus and food buff ids to static tables

--- LibCombat/LibCombat.lua
+++ LibCombat/LibCombat.lua
@@ -472,6 +472,59 @@ local validSkillEndResults = {
 
 }
 
+local foodBuffIdToItemLinks = {
+	[61218] = "|H0:item:68253:311:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Longfin Pasty with Melon Sauce
+	[61255] = "|H0:item:68247:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Braised Rabbit with Spring Vegetables
+	[61257] = "|H0:item:68243:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Melon-Baked Parmesan Pork
+	[61259] = "|H0:item:43142:139:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Argonian Saddle-Cured Rabbit
+	[61260] = "|H0:item:43154:139:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Fresh Apples and Eidar Cheese
+	[61261] = "|H0:item:68239:309:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Hearty Garlic Corn Chowder
+	[61294] = "|H0:item:68250:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Late Hearthfire Vegetable Tart
+	[61322] = "|H0:item:68257:309:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Markarth Mead
+	[61325] = "|H0:item:68260:309:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Muthsera's Remorse
+	[61328] = "|H0:item:68263:309:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Hagraven's Tonic
+	[61335] = "|H0:item:68266:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Bravil Bitter Barley Beer
+	[61340] = "|H0:item:68268:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Camlorn Sweet Brown Ale
+	[61345] = "|H0:item:68271:310:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Rosy Disposition Tonic
+	[61350] = "|H0:item:68276:311:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Lusty Argonian Maid Mazte
+	[68411] = "|H0:item:64711:123:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Crown Fortifying Meal
+	[68416] = "|H0:item:64712:123:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Crown Refreshing Drink
+	[71057] = "|H0:item:71057:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Orzorga's Tripe Trifle Pocket
+	[72822] = "|H0:item:71058:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Orzorga's Blood Price Pie
+	[72824] = "|H0:item:71059:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Orzorga's Smoked Bear Haunch
+	[84720] = "|H0:item:87695:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Ghastly Eye Bowl
+	[84731] = "|H0:item:87697:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Witchmother's Potent Brew
+	[84709] = "|H0:item:87691:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Crunchy Spider Skewer
+	[86673] = "|H0:item:112425:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Lava Foot Soup-and-Saltrice
+	[89955] = "|H0:item:120762:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Candied Jester's Coins
+	[89957] = "|H0:item:120763:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h ", 		-- Dubious Camoran Throne
+	[89971] = "|H0:item:120764:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Jewels of Misrule
+	[100498] = "|H0:item:133556:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Clockwork Citrus Filet
+	[107748] = "|H0:item:139016:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Artaeum Pickled Fish Bowl
+	[107789] = "|H0:item:139018:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Artaeum Takeaway Broth
+	[127596] = "|H0:item:153629:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 		-- Bewitched Sugar Skulls
+	[147687] = "|H0:item:171323:124:10:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h", 	-- Colovian War Torte
+}
+
+lib.foodBuffIdToItemLinks = foodBuffIdToItemLinks
+
+local MundusStones = {
+	[13975] = true,
+	[13980] = true,
+	[13943] = true,
+	[13978] = true,
+	[13976] = true,
+	[13981] = true,
+	[13982] = true,
+	[13979] = true,
+	[13940] = true,
+	[13985] = true,
+	[13977] = true,
+	[13984] = true,
+	[13974] = true,
+}
+
+lib.MundusStones = MundusStones
 
 local CustomAbilityName = {
 
